31f097967e5acd07f7f6979527052cc5636e3082
Abdellah El Kacem <ael-kace@e1r6p6.1337.ma>
2023-03-17 22:35:47 +0100

    first

--- psh_stack.c
+++ psh_stack.c
@@ -6,7 +6,7 @@
 /*   By: ael-kace <ael-kace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 20:16:46 by ael-kace          #+#    #+#             */
-/*   Updated: 2023/03/09 13:48:19 by ael-kace         ###   ########.fr       */
+/*   Updated: 2023/03/09 23:11:04 by ael-kace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,26 @@ void	psh_stk_a(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*tmp;
 
+	if (stack_size(*stack_a) > 0)
+	{
+		*stack_a = tmp;
+		tmp = *stack_b;
+		free(*stack_a);
+	}
+	free (tmp);
+	write (1, "pb\n", 1);
+}
 
-	if ()
+void	psh_stk_b(t_stack **stack_a, t_stack **stack_b)
+{
+	t_stack	*tmp;
+
+	if (stack_size(*stack_a) > 0)
+	{
+		*stack_b = tmp;
+		tmp = *stack_a;
+		free(*stack_b);
+	}
+	free (tmp);
+	write (1, "pa\n", 1);
 }
--- rot_stack.c
+++ rot_stack.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   rot_stack.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-kace <ael-kace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/08 18:26:27 by ael-kace          #+#    #+#             */
-/*   Updated: 2023/03/09 23:01:45 by ael-kace         ###   ########.fr       */
+/*   Created: 2023/03/09 23:11:24 by ael-kace          #+#    #+#             */
+/*   Updated: 2023/03/09 23:11:59 by ael-kace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct s_stack
-{
-	int				nbr;
-	struct s_stack	*next;
-}					t_stack;
-
-size_t	stack_size(t_stack *data);
-
-#endif
+#include "push_swap.h"
