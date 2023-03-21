90ce71de5810daa08b4a8bbbe38255b1d7a00de1
Abdellah El Kacem <ael-kace@e1r7p5.1337.ma>
2023-03-21 17:49:22 +0000

    first

--- utils_stack.c
+++ utils_stack.c
@@ -6,7 +6,7 @@
 /*   By: ael-kace <ael-kace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:52:21 by ael-kace          #+#    #+#             */
-/*   Updated: 2023/03/20 21:25:36 by ael-kace         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:30:03 by ael-kace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,47 @@ size_t	stack_size(t_stack *data)
 	return (size);
 }
 
-char	*join_stck(char **av)
+char	**join_sstck(char **av)
 {
-	char	*str;
 	char	*tmp;
+	char	*tmp_tmp;
+	char	**str;
 	int		i;
 
 	i = 1;
-	tmp = ft_strdup("");
+	tmp_tmp = ft_strdup("");
 	while (av[i])
 	{
-		str = ft_strjoin(tmp, av[i]);
-		free (tmp);
-		tmp = ft_strjoin(str, av[i]);
-		free(str);
+		tmp = ft_strjoin(tmp_tmp, av[i]);
+		free (tmp_tmp);
+		tmp_tmp = ft_strjoin(tmp, av[i]);
+		free(tmp);
 		i++;
 	}
+	str = ft_split(tmp_tmp, ' ');
+	free(tmp_tmp);
 	return (str);
+}
+
+char	**join_stck(char **av)
+{
+	char	**str;
+	int		i;
+
+	i = 1;
+	while (av[i])
+		str = ft_split(av[i], ' ');
+	return (str);
+}
+
+int main (int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	while (av[i])
+	{
+		printf("%s", join_stck(av));
+		i++;
+	}
 }
